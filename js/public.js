8c0b57a41376370d0384b85c44ee9bb908b401de
SontTam-tam <gacbam0315@gmail.com>
2025-06-02 15:09:29 +0900

    자바스크립트수정

--- js/public.js
+++ js/public.js
@@ -8,6 +8,12 @@ const gnbh = getAll('#header .gnb ul li h3');
 const gnbmenu = getAll('#header .gnb ul li .nav-content li ');
 const ficon = getAll('#footer .footer-icon li a img');
 const $link = getAll('a[href="#"]');
+const loginicon = getAll('#header .icon li');
+const loginbar = get('#header .login');
+const languagebar = get('#header .language');
+const search = get('.search ');
+const close = get('.search .close');
+
 import { logoObj, logoObjoff } from './data.js';
 const link = () => {
     $link.forEach((item, idx) => {
@@ -16,17 +22,22 @@ const link = () => {
 };
 let cnt = 0;
 const mainNav = () => {
-    link();
+    const header = get('#header');
+    const gnblist = getAll('#header .gnb ul .li-box');
+    const gnbul = get('#header .gnb ul');
+    const gnbh = getAll('#header .gnb ul li h3');
+    const gnbmenu = getAll('#header .gnb ul li .nav-content li ');
+    const loginbar = get('#header .login');
+    const languagebar = get('#header .language');
 
+    link();
     gnblist.forEach((item, idx) => {
         item.addEventListener('mouseenter', (e) => {
             cnt = idx;
             gnblist.forEach((item, dx) => {
                 item.classList.remove('on');
-                gnbh[dx].classList.remove('on');
             });
             gnblist[cnt].classList.add('on');
-            gnbh[cnt].classList.add('on');
             gnbh[cnt].style.color = '#fff';
         });
         item.addEventListener('mouseleave', (e) => {
@@ -37,6 +48,8 @@ const mainNav = () => {
         gnbul.addEventListener('mouseenter', (e) => {
             header.classList.add('on');
             item.classList.add('mr');
+            loginbar.classList.remove('on');
+            languagebar.classList.remove('on');
         });
         gnbul.addEventListener('mouseleave', (e) => {
             header.classList.remove('on');
@@ -54,19 +67,41 @@ const mainNav = () => {
                 item.classList.remove('on');
             }),
         ]);
-        // 메뉴 에서 마우스 호버시 색 변경부분
-        /*
-        header.addEventListener('mouseenter', (e) => {
-            header.classList.add('on');
-            item.classList.add('mr');
-        });
-        header.addEventListener('mouseleave', (e) => {
-            header.classList.remove('on');
-            item.classList.remove('mr');
-        });
-        // header 부분 색 변경 + 높이 변경부분
-        */
     });
+};
+const searchCon = () => {
+    const header = get('#header');
+    const loginicon = getAll('#header .icon li');
+    const loginbar = get('#header .login');
+    const languagebar = get('#header .language');
+    const search = get('.search ');
+    const close = get('.search .close');
+    loginicon[0].addEventListener('mouseenter', (e) => {
+        loginbar.classList.add('on');
+        languagebar.classList.remove('on');
+    });
+    loginicon[1].addEventListener('mouseenter', (e) => {
+        languagebar.classList.add('on');
+        loginbar.classList.remove('on');
+    });
+    loginicon[2].addEventListener('mouseenter', (e) => {
+        loginbar.classList.remove('on');
+        languagebar.classList.remove('on');
+    });
+    header.addEventListener('mouseleave', (e) => {
+        loginbar.classList.remove('on');
+        languagebar.classList.remove('on');
+    });
+    loginicon[2].addEventListener('click', (e) => {
+        search.classList.add('on');
+    });
+    close.addEventListener('click', (e) => {
+        search.classList.remove('on');
+    });
+};
+
+const footerNav = () => {
+    const ficon = getAll('#footer .footer-icon li a img');
     ficon.forEach((item, idx) => {
         item.addEventListener('mouseenter', (e) => {
             cnt = idx;
@@ -81,6 +116,31 @@ const mainNav = () => {
     });
 };
 
+const comInit = () => {
+    const getPage = (page, tag) => {
+        fetch(page)
+            .then((res) => res.text())
+            .then((res) => {
+                if (tag === '#header') {
+                    get(tag).innerHTML = res;
+                    mainNav();
+                }
+                if (tag === '#footer') {
+                    get(tag).innerHTML = res;
+                    footerNav();
+                }
+                if (tag === '.search') {
+                    get(tag).innerHTML = res;
+                    searchCon();
+                }
+            });
+    };
+    getPage('page/header.html', '#header');
+    getPage('page/footer.html', '#footer');
+    getPage('page/search.html', '.search');
+};
+
 (() => {
-    mainNav();
+    comInit();
+    link();
 })();
