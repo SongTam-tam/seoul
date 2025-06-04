e2bd73ecd8b06540e2bca97fd5569dd972753d2c
SongTam-tam <gacbam0315@gmail.com>
2025-06-04 15:48:02 +0900

    메인페이지 디자인중

---
+++ js/main.js
@@ -0,0 +1,55 @@
+import { get, getAll } from './get.js';
+
+const mainPage = () => {
+    const video = get('#visual .video-visual');
+    const next = get('#visual .gage .btn .next');
+    const prev = get('#visual .gage .btn .prev');
+    const stop = get('#visual .gage .btn .stop');
+    const stopi = get('#visual .gage .btn .stop i');
+    const gage = get('#visual .gage strong');
+    const visualSpan = get('#visual .gage span');
+
+    let cnt = 0,
+        timer = null,
+        interval = 20000,
+        interval2 = 20001,
+        imgurl,
+        isplay = true;
+    // gage.animate({ transform: ['scaleX(0)', 'scaleX(1)'] }, 20000);
+    gage.animate({ transform: ['scaleX(0)', 'scaleX(1)'] }, 20000);
+    visualSpan.textContent = `${cnt + 1} / 2`;
+    const banner = () => {
+        imgurl = `videos/seoulAsan${cnt}.mp4`;
+        video.setAttribute('src', imgurl);
+        gage.animate({ transform: ['scaleX(0)', 'scaleX(1)'] }, 20000);
+        visualSpan.textContent = `${cnt + 1} / 2`;
+    };
+    const visualTimer = () => {
+        cnt = cnt > 0 ? (cnt = 0) : cnt + 1;
+        banner();
+    };
+    timer = setInterval(visualTimer, interval);
+    next.addEventListener('click', (e) => {
+        cnt = cnt > 0 ? (cnt = 0) : cnt + 1;
+        banner();
+        clearInterval(visualTimer);
+        timer = setInterval(visualTimer, interval);
+    });
+    stop.addEventListener('click', (e) => {
+        if (isplay) {
+            video.pause();
+            stopi.classList.replace('xi-pause', 'xi-play');
+            clearInterval(visualTimer);
+            gage.animate({ transform: ['scaleX(0)', 'scaleX(1)'] }, 20000).pause();
+        } else {
+            video.play();
+            timer = setInterval(visualTimer, interval);
+            stopi.classList.replace('xi-play', 'xi-pause');
+        }
+        isplay = !isplay;
+    });
+};
+
+(() => {
+    mainPage();
+})();
