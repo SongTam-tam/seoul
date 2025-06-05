2784b407b8b012e587cd142fff179cf3ea7501b3
SongTam-tam <gacbam0315@gmail.com>
2025-06-05 14:43:42 +0900

    메인페이지 작업중

--- js/main.js
+++ js/main.js
@@ -77,7 +77,82 @@ const mainPage = () => {
         isplay = !isplay;
     });
 };
+const staff = () => {
+    const lis = getAll('#main .medical-staff .inner .staff .con > li');
+    const parent = get('#main .medical-staff .inner .staff .con');
+    const lisblur = getAll('#main .medical-staff .inner .staff .con li .con-box li');
+    const btn = getAll('#main .medical-staff .inner .staff .con li .con-box li button');
+
+    let first,
+        last,
+        timer = null,
+        interval = 5000,
+        cnt = 0;
+
+    const staffTimer = () => {
+        // cnt = cnt > 9 ? (cnt = 0) : cnt + 1;
+        first = parent.firstElementChild;
+        parent.append(first);
+    };
+    timer = setInterval(staffTimer, interval);
+
+    lis.forEach((item, idx) => {
+        item.addEventListener('mouseenter', (e) => {
+            clearInterval(timer);
+            item.style.top = '-55px';
+            lisblur[idx].style.top = '0px';
+        });
+        item.addEventListener('mouseleave', (e) => {
+            timer = setInterval(staffTimer, interval);
+            item.style.top = '0px';
+            lisblur[idx].style.top = '500px';
+        });
+    });
+    btn.forEach((item, idx) => {
+        item.addEventListener('mouseenter', (e) => {
+            item.classList.add('on');
+        });
+        item.addEventListener('mouseleave', (e) => {
+            item.classList.remove('on');
+        });
+    });
+};
+const therapy = () => {
+    const lis = getAll('#main .therapy .inner .medical .con li');
+    const lisA = getAll('#main .therapy .inner .medical .con li a');
+    const strong = getAll('#main .therapy .inner .medical .con li strong');
+    const p = getAll('#main .therapy .inner .medical .con li p');
+
+    let cnt = 0,
+        imgurl;
+
+    lis.forEach((item, idx) => {
+        item.addEventListener('mouseenter', (e) => {
+            cnt = idx;
+            lisA.forEach((item, dx) => {
+                imgurl = `url(images/therapy${dx}-black.png)`;
+                item.style.backgroundImage = imgurl;
+                strong[dx].classList.remove('on');
+                p[dx].classList.remove('on');
+            });
+            imgurl = `url(images/therapy${cnt}.png)`;
+            lisA[cnt].style.backgroundImage = imgurl;
+            strong[cnt].classList.add('on');
+            p[cnt].classList.add('on');
+        });
+        item.addEventListener('mouseleave', (e) => {
+            lisA[0].style.backgroundImage = `url(images/therapy0.png)`;
+            lisA[1].style.backgroundImage = `url(images/therapy1.png)`;
+            lisA[2].style.backgroundImage = `url(images/therapy2.png)`;
+            lisA[3].style.backgroundImage = `url(images/therapy3.png)`;
+            strong[cnt].classList.remove('on');
+            p[cnt].classList.remove('on');
+        });
+    });
+};
 
 (() => {
     mainPage();
+    staff();
+    therapy();
 })();
